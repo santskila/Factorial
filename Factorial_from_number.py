5878d0f137214f957fcd22ae33023ed260f2bd8e
santskila <bielferreirasd@gmail.com>
2024-11-09 23:02:31 -0300

    Some comments
    add some comments, to make it easier to know what each line does

--- Factorial_from_number.py
+++ Factorial_from_number.py
@@ -1,13 +1,12 @@
-print('This program will calculate the factorial of any number')
+print('This program will calculate the factorial of any number') #Show what the program does
 
-number = int(input("Type your number here: "))
-
+number = int(input("Type your number here: ")) #Gets the number from the user
 
 factorial = 1
 
-for i in range(number + 1):
+for i in range(number + 1): #Calculates the factorial
     if i == 0:
         continue
     factorial = factorial*i
 
-print(f'Your factorial is: {factorial}')
+print(f'Your factorial is: {factorial}') #Show the results
