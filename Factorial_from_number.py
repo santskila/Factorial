e782113f80c4bd54ebed101e684f3e76cd9dbf04
santskila <bielferreirasd@gmail.com>
2024-11-09 23:07:30 -0300

    Made some otimizations
    I removed the 'if' and 'continue' and changed the multiplication to *=

--- Factorial_from_number.py
+++ Factorial_from_number.py
@@ -4,9 +4,7 @@ number = int(input("Type your number here: ")) #Gets the number from the user
 
 factorial = 1
 
-for i in range(number + 1): #Calculates the factorial
-    if i == 0:
-        continue
-    factorial = factorial*i
+for i in range(1, number + 1): #Calculates the factorial
+    factorial *= i
 
 print(f'Your factorial is: {factorial}') #Show the results
